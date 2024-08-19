e7d38bf1fe41d930d9685aa9630cc056a6a8aa0d
Francisco Araujo <francisco@opiagile.com>
2024-08-19 19:52:34 -0400

    Initial commit with Spring Boot setup.

---
+++ src/test/java/com/francisco/awards/goldenraspberryawards/GoldenRaspberryAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.awards.goldenraspberryawards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
